e678f5c9fb2b639f3b72af8a5881418e2a3bc51c
vovantus <vshirokov@gmail.com>
2024-08-01 13:53:48 +0200

    mui theme fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,8 @@ import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "../theme";
-import { Roboto } from "next/font/google";
-
-const roboto = Roboto({
-  weight: ["300", "400", "500", "700"],
-  subsets: ["latin", "cyrillic"],
-  display: "swap",
-});
+import theme from "@/theme";
+import { CssBaseline } from "@mui/material";
 
 export const metadata: Metadata = {
   title: "Proflorist app",
@@ -19,9 +13,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
--- theme.ts
+++ theme.ts
@@ -1,7 +1,16 @@
 "use client";
 
+
+
+import { Roboto } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 declare module "@mui/material/styles" {
   interface BreakpointOverrides {
@@ -42,10 +51,13 @@ const theme = createTheme({
       main: red.A400,
     },
   },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
   components: {
     MuiCssBaseline: {
       styleOverrides: `
-        html {
+        body {
           overflow-y: scroll;
         }
       `,
