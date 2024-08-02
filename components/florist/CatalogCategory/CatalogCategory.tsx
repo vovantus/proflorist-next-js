58039c194ad80b2f06469096027c98a0d3bed26d
vovantus <vshirokov@gmail.com>
2024-08-02 14:55:25 +0200

    category list fix for mobile

--- components/florist/CatalogCategory/CatalogCategory.tsx
+++ components/florist/CatalogCategory/CatalogCategory.tsx
@@ -1,5 +1,5 @@
 import Category from "@/lib/types/Category";
-import { Card, CardContent, Typography, Box } from "@mui/material";
+import { Card, CardContent, Typography, Box, Button } from "@mui/material";
 import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
 import Link from "next/link";
 
@@ -13,34 +13,27 @@ export default function CatalogCategory({
   parentUrl,
 }: CatalogCategoryProps) {
   return (
-    <Card
+    <Button
+      endIcon={<ChevronRightOutlinedIcon />}
+      component={Link}
+      variant="contained"
+      href={parentUrl ? `${parentUrl}/${category.slug}` : category.slug}
       sx={{
         minWidth: 350,
         textDecoration: "none",
         backgroundColor: "#f5f5f5",
+        textTransform: "none",
+        pt: 2,
+        pb: 3,
+        justifyContent: "space-between",
         "&:hover": {
           backgroundColor: "rgba(118, 202, 254, 0.5)",
-          transition: "background-color 0.3s ease",
         },
       }}
-      component={Link}
-      href={parentUrl ? `${parentUrl}/${category.slug}` : category.slug}
     >
-      <CardContent>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography variant="h5" component="div">
-            {category.name}
-          </Typography>
-          <ChevronRightOutlinedIcon />
-        </Box>
-      </CardContent>
-    </Card>
+      <Typography variant="h5" component="div">
+        {category.name}
+      </Typography>
+    </Button>
   );
 }
