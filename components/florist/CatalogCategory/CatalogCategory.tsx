23e7250c64b610113aa7147795f7d2be8f6151fb
vovantus <vshirokov@gmail.com>
2024-08-07 10:28:44 +0200

    one more try for ios chrome

--- components/florist/CatalogCategory/CatalogCategory.tsx
+++ components/florist/CatalogCategory/CatalogCategory.tsx
@@ -26,9 +26,16 @@ export default function CatalogCategory({
         pt: 2,
         pb: 3,
         justifyContent: "space-between",
+        "&:hover": {
+          backgroundColor: "#f5f5f5",
+          boxShadow:
+            "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)",
+        },
         "@media (hover: hover)": {
           "&:hover": {
             backgroundColor: "#98d9ff",
+            boxShadow:
+              "0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12)",
           },
         },
       }}
